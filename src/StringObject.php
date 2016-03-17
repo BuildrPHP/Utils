c6e0eea7340083436b28ef4adc656ae11152725a
Zoltán Borsos <zolli07@gmail.com>
2016-03-17 21:47:18 +0100

    *Finish

--- src/StringObject.php
+++ src/StringObject.php
@@ -2,17 +2,13 @@
 
 use BuildR\Foundation\Exception\InvalidArgumentException;
 use BuildR\Foundation\Object\StringConvertibleInterface;
-use BuildR\Utils\Exception\IntegrationException;
-use BuildR\Utils\Integration\BuiltInStringFunctionsIntegration;
 
 class StringObject implements StringConvertibleInterface {
 
-    private $string;
-
     /**
-     * @type \BuildR\Utils\Integration\BuiltInStringFunctionsIntegration
+     * @type string
      */
-    private $builtInIntegration;
+    private $string;
 
     public function __construct($string) {
         if(!is_string($string)) {
@@ -20,24 +16,6 @@ class StringObject implements StringConvertibleInterface {
         }
 
         $this->string = $string;
-    }
-
-    public function __call($name, $arguments) {
-        if($this->builtInIntegration === NULL) {
-            $this->builtInIntegration = new BuiltInStringFunctionsIntegration();
-        }
-
-        if($this->builtInIntegration->hasMethod($name)) {
-            array_unshift($arguments, $this->string);
-            $result = $this->builtInIntegration->run($name, $arguments);
-
-            $newObject = clone $this;
-            $newObject->string = $result;
-
-            return $newObject;
-        }
-
-        throw IntegrationException::nonExistingMethod(get_class($this->builtInIntegration), $name);
     }
 
     /**
